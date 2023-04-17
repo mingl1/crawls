f0f1fb9b7244a261abae4e2504b68c43e9341adb
mingl1 <linm1@bxscience.edu>
2023-04-17 12:11:11 -0400

    vercel analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -7,7 +8,9 @@ export default function RootLayout({ children }) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
       </head>
-      <body className="">{children}</body>
+      <body className="">
+        {children} <Analytics />
+      </body>
     </html>
   );
 }
