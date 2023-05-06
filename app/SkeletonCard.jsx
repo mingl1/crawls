83306950836d856b362631bfae4d7bd2439e3e81
linm1 <linm1@bxscience.edu>
2023-05-06 16:19:43 -0400

    fix margin of skeletoncard top

--- app/SkeletonCard.jsx
+++ app/SkeletonCard.jsx
@@ -5,7 +5,10 @@ export default function SkeletonCard() {
   return Array(4)
     .fill(0)
     .map((_, i) => (
-      <div className={`flex lg:p-5 md:p-5 my-5`} key={i + 33}>
+      <div
+        className={`flex lg:p-5 md:p-5 ${i != 0 ? "my-5" : "mb-5"}}`}
+        key={i + 33}
+      >
         <Skeleton className="w-1/2 object-cover flex-1 min-w-[192px] rounded-md h-32" />
         <div className="flex-1 overflow-hidden text-center p-1">
           <Skeleton className="mx-auto" width={"70%"} />
